d1b434a1da08247e275bef326f00fd7f5964c6f6
saji-spoon-win8-msys2 <tkgkmny0707@hotmail.co.jp>
2017-11-18 02:25:09 +0900

    First Commit

---
+++ MatImageConvert.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include<Siv3D.hpp>
+#include<opencv2/opencv.hpp>
+
+namespace cvsiv
+{
+        /// <summary>
+        /// s3d::Image内のデータを参照するcv::Matを作成します。
+        /// </summary>
+        /// <param name="image">参照するs3d::Image</param>
+        /// <returns>cv::Mat</returns>
+        cv::Mat ImageToMat(s3d::Image& image);
+
+        /// <summary>
+        /// <para>下記の条件を満たすcv::Matからs3d::Imageを新しく作成します。</para>
+        /// <para>・次元数2</para>
+        /// <para>・ビット深度8</para>
+        /// <para>・チャンネル数4でRGBA</para>
+        /// <para>・連続データである</para>
+        /// </summary>
+        /// <param name="mat">作成元のcv::Mat</param>
+        /// <returns>s3d::Image</returns>
+        s3d::Image MatToImage(const cv::Mat& mat);
+
+        /// <summary>
+        /// cv::Matからs3d::Imageを新しく作成します。
+        /// </summary>
+        /// <param name="mat">作成元のcv::Mat</param>
+        /// <param name="isBGR">画素並び(BGR→true, RGB→false) グレイスケールの場合は無視します</param>
+        /// <returns>s3d::Image</returns>
+        s3d::Image MatToImageForce(const cv::Mat& mat, bool isBGR);
+}
