c438d1545c0bc99d2f042ea28f122e12a560ab6a
saji-spoon <tkgkmny0707@hotmail.co.jp>
2017-12-04 21:39:47 +0900

    Rename and Add/Change Functions for Proper Use

--- MatImageConvert.cpp
+++ MatImageConvert.cpp
@@ -1,11 +1,69 @@
 #include"MatImageConvert.hpp"
 
-cv::Mat cvsiv::ImageToMat(s3d::Image& image)
+cv::Mat cvsiv::GetMatLinkedToImage(s3d::Image& image)
 {
         cv::Mat_<cv::Vec4b> mat(image.height, image.width, static_cast<cv::Vec4b*>(image.data()), image.stride);
         
         return mat;
 }
+
+cv::Mat cvsiv::ImageToMat(s3d::Image& image)
+{
+        //引数のimageを参照するcv::Matを作成
+        cv::Mat linkedMat = GetMatLinkedToImage(image);
+
+        //データをコピー
+        cv::Mat mat = linkedMat.clone();
+
+        return mat;
+}
+
+bool cvsiv::IsPossibleMatToImageForce(const cv::Mat& mat)
+{
+        if (mat.data == nullptr)
+        {
+                LOG_ERROR(L"IsPossibleMatToImageForce: cv::Mat is empty.");
+                return false;
+        }
+
+        if (mat.dims != 2)
+        {
+                LOG_ERROR(L"IsPossibleMatToImageForce: cv::Mat dimension must be 2.");
+                return false;
+        }
+
+        switch (mat.channels())
+        {
+        case 1: case 3: case 4:
+                break;
+        default:
+                LOG_ERROR(L"IsPossibleMatToImageForce: cv::Mat channel must be 1, 3 or 4.");
+                return false;
+                break;
+        }
+
+        return true;
+}
+
+bool cvsiv::IsPossibleMatToImage(const cv::Mat& mat)
+{
+        if (!IsPossibleMatToImageForce(mat)) return false;
+
+        if (mat.channels() != 4)
+        {
+                LOG_ERROR(L"IsPossibleMatToImage: cv::Mat channel must be 4.");
+                return false;
+        }
+
+        if (mat.depth() != CV_8U)
+        {
+                LOG_ERROR(L"IsPossibleMatToImage: cv::Mat depth must be CV_8U");
+                return false;
+        }
+
+        return true;
+}
+
 
 s3d::Image cvsiv::MatToImage(const cv::Mat& mat)
 {
@@ -18,19 +76,9 @@ s3d::Image cvsiv::MatToImage(const cv::Mat& mat)
         return image;
 }
 
-s3d::Image cvsiv::MatToImageForce(const cv::Mat& mat, bool isBGR)
+s3d::Image cvsiv::MatToImageForce(const cv::Mat& mat, bool isRGB)
 {
-        if (mat.data == nullptr)
-        {
-                return s3d::Image();
-        }
-
-        if (mat.dims != 2)
-        {
-                LOG_ERROR(L"MatToImage: cv::Mat dimension must be 2.");
-                return s3d::Image();
-
-        }
+        if (!IsPossibleMatToImageForce(mat)) return s3d::Image();        
 
         cv::Mat copy = mat.clone();
 
@@ -42,14 +90,10 @@ s3d::Image cvsiv::MatToImageForce(const cv::Mat& mat, bool isBGR)
                 cv::cvtColor(copy, copy, cv::COLOR_GRAY2RGBA);
                 break;
         case 3:
-                cv::cvtColor(copy, copy, isBGR ? cv::COLOR_BGR2RGBA : cv::COLOR_RGB2RGBA);
+                cv::cvtColor(copy, copy, isRGB ? cv::COLOR_RGB2RGBA : cv::COLOR_BGR2RGBA);
                 break;
         case 4:
-                if (isBGR) cv::cvtColor(copy, copy, cv::COLOR_BGRA2RGBA);
-                break;
-        default:
-                LOG_ERROR(L"MatToImage: cv::Mat channel must be 1, 3 or 4.");
-                return s3d::Image();
+                if (!isRGB) cv::cvtColor(copy, copy, cv::COLOR_BGRA2RGBA);
                 break;
         }
 
--- MatImageConvert.hpp
+++ MatImageConvert.hpp
@@ -10,24 +10,45 @@ namespace cvsiv
         /// </summary>
         /// <param name="image">参照するs3d::Image</param>
         /// <returns>cv::Mat</returns>
+        cv::Mat GetMatLinkedToImage(s3d::Image& image);
+
+        /// <summary>
+        /// s3d::Image内のデータをコピーしたcv::Matを作成します。
+        /// </summary>
+        /// <param name="image">コピーするs3d::Image</param>
+        /// <returns>cv::Mat</returns>
         cv::Mat ImageToMat(s3d::Image& image);
 
         /// <summary>
+        /// 引数のcv::MatがMatToImageForceを適用できるかどうか返します。
+        /// </summary>
+        /// <param name="image">検査するcv::Mat</param>
+        /// <returns>適用可能ならtrue, 不可ならfalse</returns>
+        bool IsPossibleMatToImageForce(const cv::Mat& mat);
+
+        /// <summary>
+        /// 引数のcv::MatがMatToImageを適用できるかどうか返します。
+        /// </summary>
+        /// <param name="image">検査するcv::Mat</param>
+        /// <returns>適用可能ならtrue, 不可ならfalse</returns>
+        bool IsPossibleMatToImage(const cv::Mat& mat);
+
+        /// <summary>
         /// <para>下記の条件を満たすcv::Matからs3d::Imageを新しく作成します。</para>
-        /// <para>・次元数2</para>
         /// <para>・ビット深度8</para>
         /// <para>・チャンネル数4でRGBA</para>
-        /// <para>・連続データである</para>
         /// </summary>
         /// <param name="mat">作成元のcv::Mat</param>
         /// <returns>s3d::Image</returns>
         s3d::Image MatToImage(const cv::Mat& mat);
 
         /// <summary>
-        /// cv::Matからs3d::Imageを新しく作成します。
+        /// cv::Matからs3d::Imageを新しく作成します。（チャンネル数やビット深度の違いを吸収）
         /// </summary>
         /// <param name="mat">作成元のcv::Mat</param>
         /// <param name="isBGR">画素並び(BGR→true, RGB→false) グレイスケールの場合は無視します</param>
         /// <returns>s3d::Image</returns>
         s3d::Image MatToImageForce(const cv::Mat& mat, bool isBGR);
+
+
 }
